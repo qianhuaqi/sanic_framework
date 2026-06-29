9d5cd5f09d295771dce60b3a0c51fff8e2d085f6
qianhuaqi <18610088892@163.com>
2026-06-29 16:18:27 +0800

    fix(core): restore public root facade
    Signed-off-by: bug- <18610088892@163.com>

--- lingshu/__init__.py
+++ lingshu/__init__.py
@@ -1,8 +1,17 @@
 """LingShu public application facade.
 
-Public framework objects are introduced by later P1 issues. Importing this package has no
-runtime side effects: it does not start tasks, open files, bind sockets, connect to
-services, or import user applications.
+Importing this package has no runtime side effects: it does not start tasks, open files,
+bind sockets, connect to services, or import user applications.
 """
 
-__all__: tuple[str, ...] = ()
+from lingshu.core.application import LingShu
+from lingshu.http.exceptions import HTTPException
+from lingshu.http.request import Request
+from lingshu.http.response import Response
+
+__all__ = (
+    "HTTPException",
+    "LingShu",
+    "Request",
+    "Response",
+)
